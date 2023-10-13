bff43ba556c2dbcc7f6432171362070d2ba317b9
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-10-13 19:59:17 +0300

    Routing

---
+++ S21-ReactRouter/src/pages/Home.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <Link to="/products">Products</Link>
+      </p>
+    </>
+  );
+};
+
+export default HomePage;
