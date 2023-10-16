1c6da91c04b1a741761f1f5f004a3ef08165d328
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-10-16 13:09:36 +0300

    Initial

--- S21-ReactRouter/src/pages/Products.js
+++ S21-ReactRouter/src/pages/Products.js
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+  { id: "p1", title: "Product 1" },
+  { id: "p2", title: "Product 2" },
+  { id: "p3", title: "Product 3" },
+];
+
 const ProductsPage = () => {
-  return <h1>Products</h1>;
+  return (
+    <>
+      <h1>Products</h1>
+      <ul>
+        {PRODUCTS.map((p) => (
+          <li key={p.id}>
+            <Link to={p.id}>{p.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default ProductsPage;
