48e89c93e6da41416a39ccad3ea5c93dab0e9c73
naiden00 <naiden.petrov.31.12.00@gmail.com>
2024-12-25 16:37:02 +0200

    react form actions

--- S18-FormActions/06-adv-starting-project/src/components/Opinion.jsx
+++ S18-FormActions/06-adv-starting-project/src/components/Opinion.jsx
@@ -1,4 +1,30 @@
+import { use } from "react";
+import { OpinionsContext } from "../store/opinions-context";
+import { useActionState } from "react";
+import { useOptimistic } from "react";
+
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
+  const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
+  const [optimisticVotes, setVotesOptimistically] = useOptimistic(
+    votes,
+    (prevVotes, mode) => (mode === "up" ? prevVotes + 1 : prevVotes - 1)
+  );
+  const uploadAction = async () => {
+    setVotesOptimistically("up");
+    await upvoteOpinion(id);
+  };
+  const downloadAction = async () => {
+    setVotesOptimistically("down");
+    await downvoteOpinion(id);
+  };
+
+  const [upvoteFormState, upvoteFormAction, upvotePending] = useActionState(
+    uploadAction,
+    null
+  );
+  const [downvoteFormState, downvoteFormAction, downvotePending] =
+    useActionState(downloadAction, null);
+
   return (
     <article>
       <header>
@@ -7,7 +33,10 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
       </header>
       <p>{body}</p>
       <form className="votes">
-        <button>
+        <button
+          formAction={upvoteFormAction}
+          disabled={upvotePending || downvotePending}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -25,9 +54,12 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
           </svg>
         </button>
 
-        <span>{votes}</span>
+        <span>{optimisticVotes}</span>
 
-        <button>
+        <button
+          formAction={downvoteFormAction}
+          disabled={upvotePending || downvotePending}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
